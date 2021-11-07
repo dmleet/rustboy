73ef184a0bc18bdca012f454e0c2279ed8c276ed
Daniel Leet <dmleet1@gmail.com>
2021-11-07 16:20:49 -0500

    rework alu to use Register::set_flags()

--- src/alu.rs
+++ src/alu.rs
@@ -22,25 +22,50 @@ impl Carry for u8 {
     }
 }
 
-// 8-bit Functions
+/// 8-bit Functions
 
-// Stores the result of ADD n in A and sets flags
+// Add A, n
 pub fn alu_add(reg: &mut Registers, n: u8) {
-    let r = reg.a.carry_add(n);
-    reg.set_flag(Flag::Z, r.0 == 0);
-    reg.set_flag(Flag::N, false);
-    reg.set_flag(Flag::H, r.1);
-    reg.set_flag(Flag::C, r.2);
-    reg.a = r.0;
+    let (r, h, c) = reg.a.carry_add(n);
+    reg.set_flags(r == 0, false, h, c);
+    reg.a = r;
 }
 
-// Returns the result of AND n and sets flags
+// ADC A, n
+pub fn alu_adc(reg: &mut Registers, n: u8) {
+    let c: u8 = reg.get_flag_bit(Flag::C);
+    alu_add(reg, n + c);
+}
+
+// SUB A, n
+pub fn alu_sub(reg: &mut Registers, n: u8) {
+    let (r, h, c) = reg.a.carry_sub(n);
+    reg.set_flags(r == 0, true, h, c);
+    reg.a = r;
+}
+
+// SBC A, n
+pub fn alu_sbc(reg: &mut Registers, n: u8) {
+    let c: u8 = reg.get_flag_bit(Flag::C);
+    alu_sub(reg, n + c);
+}
+
+// AND n
 pub fn alu_and(reg: &mut Registers, n: u8) {
     reg.a &= n;
-    reg.set_flag(Flag::Z, reg.a == 0);
-    reg.set_flag(Flag::N, false);
-    reg.set_flag(Flag::H, true);
-    reg.set_flag(Flag::C, false);
+    reg.set_flags(reg.a == 0, false, true, false);
+}
+
+// OR n
+pub fn alu_or(reg: &mut Registers, n: u8) {
+    reg.a |= n;
+    reg.set_flags(reg.a == 0, false, false, false);
+}
+
+// XOR n
+pub fn alu_xor(reg: &mut Registers, n: u8) {
+    reg.a ^= n;
+    reg.set_flags(reg.a == 0, false, false, false);
 }
 
 // BIT b, r
@@ -52,70 +77,29 @@ pub fn alu_bit(reg: &mut Registers, b: u8, r: u8) {
 
 // Sets flags from CP n
 pub fn alu_cp(reg: &mut Registers, n: u8) {
-    let r = reg.a.carry_sub(n);
-    reg.set_flag(Flag::Z, r.0 == 0);
-    reg.set_flag(Flag::N, true);
-    reg.set_flag(Flag::H, r.1);
-    reg.set_flag(Flag::C, r.2);
-}
-
-// Returns the result of DEC n and sets flags
-pub fn alu_dec(reg: &mut Registers, n: u8) -> u8 {
-    let r = n.carry_sub(1);
-    reg.set_flag(Flag::Z, r.0 == 0);
-    reg.set_flag(Flag::N, true);
-    reg.set_flag(Flag::H, r.1);
-    r.0
+    let (r, h, c) = reg.a.carry_sub(n);
+    reg.set_flags(r == 0, true, h, c);
 }
 
 // Returns the result of INC n and sets flags
 pub fn alu_inc(reg: &mut Registers, n: u8) -> u8 {
-    let r = n.carry_add(1);
-    reg.set_flag(Flag::Z, r.0 == 0);
+    let (r, h, _) = n.carry_add(1);
+    reg.set_flag(Flag::Z, r == 0);
     reg.set_flag(Flag::N, false);
-    reg.set_flag(Flag::H, r.1);
-    r.0
+    reg.set_flag(Flag::H, h);
+    r
 }
 
-// Stores the reult of OR n in A and sets flags
-pub fn alu_or(reg: &mut Registers, n: u8) {
-    reg.a = reg.a | n;
-    reg.set_flag(Flag::Z, reg.a == 0);
-    reg.set_flag(Flag::N, false);
-    reg.set_flag(Flag::H, false);
-    reg.set_flag(Flag::C, false);
-}
-
-pub fn alu_sbc(reg: &mut Registers, n: u8) {
-    let c = reg.get_flag(Flag::C) as u8;
-    let r = reg.a.carry_sub(n + c);
-    reg.set_flag(Flag::Z, r.0 == 0);
+// Returns the result of DEC n and sets flags
+pub fn alu_dec(reg: &mut Registers, n: u8) -> u8 {
+    let (r, h, _) = n.carry_sub(1);
+    reg.set_flag(Flag::Z, r == 0);
     reg.set_flag(Flag::N, true);
-    reg.set_flag(Flag::H, r.1);
-    reg.set_flag(Flag::C, r.2);
-    reg.a = r.0;
+    reg.set_flag(Flag::H, h);
+    r
 }
 
-// Stores the result of SUB n in A and sets flags
-pub fn alu_sub(reg: &mut Registers, n: u8) {
-    let r = reg.a.carry_sub(n);
-    reg.set_flag(Flag::Z, r.0 == 0);
-    reg.set_flag(Flag::N, true);
-    reg.set_flag(Flag::H, r.1);
-    reg.set_flag(Flag::C, r.2);
-    reg.a = r.0;
-}
-
-// Stores the result of XOR n in A and sets flags
-pub fn alu_xor(reg: &mut Registers, n: u8) {
-    reg.a = reg.a ^ n;
-    reg.set_flag(Flag::Z, reg.a == 0);
-    reg.set_flag(Flag::N, false);
-    reg.set_flag(Flag::H, false);
-    reg.set_flag(Flag::C, false);
-}
-
-// 16-bit Functions
+/// 16-bit Functions
 
 // Adds n to HL and sets flags
 pub fn alu_add_hl(reg: &mut Registers, n: u16) {
@@ -126,33 +110,139 @@ pub fn alu_add_hl(reg: &mut Registers, n: u16) {
     reg.set_hl(r);
 }
 
+/// Bit Opcodes
+
+// SET b, r
+pub fn alu_set(r: &mut u8, b: u8) {
+    *r |= 1 << b;
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_alu_cp() {
+    fn test_carry_add() {
         let mut reg = Registers::new();
-        reg.a =		0b11010010;
-        let n: u8 =	0b00101001;
-        alu_cp(&mut reg, n);
-		assert!(!reg.get_flag(Flag::Z));
-		assert!(reg.get_flag(Flag::N));
-		assert!(reg.get_flag(Flag::H));
-		assert!(!reg.get_flag(Flag::C));
+        reg.a = 0xFF;
+        let (r, h, c) = reg.a.carry_add(0x01);
+        assert_eq!(r, 0x00);
+        assert_eq!(h, true);
+        assert_eq!(c, true);
     }
 
-	#[test]
-	fn test_alu_cp_2() {
-		let mut reg = Registers::new();
-		reg.a =		0b01011001;
-        let n: u8 =	0b10000100;
-        alu_cp(&mut reg, n);
-		assert!(!reg.get_flag(Flag::Z));
-		assert!(reg.get_flag(Flag::N));
-		assert!(!reg.get_flag(Flag::H));
-		assert!(reg.get_flag(Flag::C));
-	}
+    #[test]
+    fn test_carry_sub() {
+        let mut reg = Registers::new();
+        reg.a = 0x00;
+        let (r, h, c) = reg.a.carry_sub(0xFF);
+        assert_eq!(r, 0x01);
+        assert_eq!(h, true);
+        assert_eq!(c, true);
+    }
+
+    #[test]
+    fn test_alu_add() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_add(&mut reg, 0x02);
+        assert_eq!(reg.a, 0x03);
+        assert_eq!(reg.f, 0);
+    }
+
+    #[test]
+    fn test_alu_adc() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_adc(&mut reg, 0x02);
+        assert_eq!(reg.a, 0x04);
+        assert_eq!(reg.f, 0);
+    }
+
+    #[test]
+    fn test_alu_sub() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_sub(&mut reg, 0x02);
+        assert_eq!(reg.a, 0xFF);
+        assert_eq!(reg.f, 0b01110000);
+    }
+
+    #[test]
+    fn test_alu_sbc() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_sbc(&mut reg, 0x02);
+        assert_eq!(reg.a, 0xFE);
+        assert_eq!(reg.f, 0b01110000);
+    }
+
+    #[test]
+    fn test_alu_and() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_and(&mut reg, 0x02);
+        assert_eq!(reg.a, 0x00);
+        assert_eq!(reg.f, 0b10100000);
+    }
+
+    #[test]
+    fn test_alu_or() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_or(&mut reg, 0x03);
+        assert_eq!(reg.a, 0x03);
+        assert_eq!(reg.f, 0);
+    }
+
+    #[test]
+    fn test_alu_xor() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_xor(&mut reg, 0x03);
+        assert_eq!(reg.a, 0x02);
+        assert_eq!(reg.f, 0);
+    }
+
+    #[test]
+    fn test_alu_cp() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        alu_cp(&mut reg, 0x02);
+        assert_eq!(reg.a, 0x01);
+        assert_eq!(reg.f, 0b01110000);
+    }
+
+    #[test]
+    fn test_alu_inc() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        let n = reg.a;
+        reg.a = alu_inc(&mut reg, n);
+        assert_eq!(reg.a, 0x02);
+        assert_eq!(reg.f, 0b00010000);
+    }
+
+    #[test]
+    fn test_alu_dec() {
+        let mut reg = Registers::new();
+        reg.f = 0b11110000;
+        reg.a = 0x01;
+        let n = reg.a;
+        reg.a = alu_dec(&mut reg, n);
+        assert_eq!(reg.a, 0x00);
+        assert_eq!(reg.f, 0b11010000);
+    }
 
 	#[test]
 	fn test_alu_add_hl() {
@@ -165,14 +255,10 @@ mod tests {
 		assert!(!reg.get_flag(Flag::C));
 	}
 
-	#[test]
-	fn test_alu_add_hl_2() {
-		let mut reg = Registers::new();
-        reg.set_hl(0xF000);
-        let n =	0xFFFF;
-		alu_add_hl(&mut reg, n);
-		assert!(!reg.get_flag(Flag::N));
-		assert!(!reg.get_flag(Flag::H));
-		assert!(reg.get_flag(Flag::C));
-	}
+    #[test]
+    fn test_alu_set() {
+        let mut r = 0x01;
+        alu_set(&mut r, 1);
+        assert_eq!(r, 0x03);
+    }
 }
